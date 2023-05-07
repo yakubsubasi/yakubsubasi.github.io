d668164108edfbe40affb53d0b6bc3921618bfc7
yakubsubasi <yakub5896@gmail.com>
2023-05-07 16:58:40 +0300

    2nd

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 // Smooth scrolling for anchor links
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
